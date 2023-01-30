ab040d67451b564fd3665394d7770f949ea925ee
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-01-30 15:29:18 +0100

    Add Todo Endpoints
    Created minimal API endpoints for Todo calls

---
+++ MinimalApi/Endpoints/TodoEndpoints.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoLibrary.Data;
+
+namespace MinimalApi.Endpoints;
+
+public static class TodoEndpoints
+{
+
+    public static void AddTodoEndpoints(this WebApplication app)
+    {
+        app.MapGet("/api/Todos", GetAllTodos);
+
+        app.MapPost("/api/Todos", CreateTodo);
+
+        app.MapDelete("/api/Todos/{id}", DeleteTodo);
+    }
+
+
+
+    private async static Task<IResult> GetAllTodos(ITodoData data)
+    {
+        var output = await data.GetAllAssigned(1);
+
+        return Results.Ok(output);
+    }
+
+
+    private async static Task<IResult> CreateTodo(ITodoData data, [FromBody] string task)
+    {
+        var output = await data.CreateTodo(1, task);
+
+        return Results.Ok(output);
+
+    }
+
+
+    private async static Task<IResult> DeleteTodo(ITodoData data, int id)
+    {
+        await data.DeleteTodo(1, id);
+
+        return Results.Ok();
+
+    }
+}
