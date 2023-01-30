30b9f301fc8785f699232e6739917eec509c03b8
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-01-30 14:56:48 +0100

    Update Endpoints
    removed Weather forecast endpoint and added two sample minimal api get endpoints

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -14,29 +14,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapGet("/api/Todo", () => new string[] { "string1", "string2" });
+app.MapGet("/api/Todo/{id}", (int id) => $"Id: {id}");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
